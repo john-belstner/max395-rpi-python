22d98b380cc76676d12f379e621768a302a09a0d
John Belstner <john.belstner@intel.com>
2019-12-03 16:08:07 -0700

    Bug fixes

--- max395.py
+++ max395.py
@@ -14,47 +14,40 @@
 import sys
 import time
 import spidev
-import os.path
 
 # Check for three arguements
-if (len(sys.argv) != 4):
+if (len(sys.argv) != 2):
     print("")
-    print("USAGE: python max395.py <cs> <bit> <state>")
-    print("WHERE: cs    = 0 or 1")
-    print("       bit   = 0, 1, 2, 3, 4, 5, 6 or 7")
-    print("       state = 0 or 1")
+    print("USAGE: python max395.py <bit>")
+    print("WHERE: bit   = 0, 1, 2, 3, 4, 5, 6 or 7")
     print("")
     sys.exit(1)
 
 # Assign the arguments to variables
-cs = int(sys.argv[1])
-bit = int(sys.argv[2])
-state = int(sys.argv[3])
-
-# Check if the previous state was saved
-if os.path.isfile('last-state.txt'):
-    file = open("last-state.txt","r")
-    last_state = int(file.read(1))
-    file.close()
-else:
-    last_state = 0
+bus = 0
+cs = 0
+bit = int(sys.argv[1])
 
 # Figure out the new state
-mask = 1 << bit
-value = state << bit
-new_state = (last_state ^ mask) | value
-
-# Save the new state
-file = open("last-state.txt","w")
-file.write(str(new_state) + "\n")
-file.close()
+value = 1 << bit
 
 # Write it to the SPI device
-spi = spidev.SpiDev(0, cs)
+spi = spidev.SpiDev(bus, cs)
 spi.max_speed_hz = 1200000
-msg = new_state.to_bytes(1, 'big')
+
+msg = value.to_bytes(1, 'big')
 try:
-    print("writing " + "{0:08b}".format(new_state) + " to SPI" + str(cs))
+    print("writing " + "{0:08b}".format(value) + " to SPI" + str(cs))
+    spi.xfer2(msg)
+except:
+    print("An exception occurred writing to SPI" + str(cs))
+
+time.sleep(.1)
+
+value = 0
+msg = value.to_bytes(1, 'big')
+try:
+    print("writing " + "{0:08b}".format(value) + " to SPI" + str(cs))
     spi.xfer2(msg)
 except:
     print("An exception occurred writing to SPI" + str(cs))
